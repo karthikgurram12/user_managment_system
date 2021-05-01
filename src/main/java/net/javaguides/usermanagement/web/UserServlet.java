ab5160d83b02024281f7ce0d05268129b91d8e6d
karthikgurram12 <chowdarykarthik0@gmail.com>
2021-05-01 19:34:54 +0530

    Update UserServlet.java

--- src/main/java/net/javaguides/usermanagement/web/UserServlet.java
+++ src/main/java/net/javaguides/usermanagement/web/UserServlet.java
@@ -34,8 +34,6 @@ public class UserServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	
-		
 		String action=request.getServletPath();
 		
 		switch(action) {
